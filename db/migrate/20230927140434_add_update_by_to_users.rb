6cf76d855a526517971f98ed3ce5037c946bc962
岩橋拓海 <minton@kin.local>
2023-09-27 23:10:15 +0900

    コメントの追加

--- db/migrate/20230927140434_add_update_by_to_users.rb
+++ db/migrate/20230927140434_add_update_by_to_users.rb
@@ -1,5 +1,5 @@
 class AddUpdateByToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :update_by, :integer
+    add_column :users, :update_by, :integer,     comment: '対応した管理者のid'
   end
 end
