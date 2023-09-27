ddd7a45ff63d6ade9686f1420cf7141ef3fcded1
岩橋拓海 <minton@kin.local>
2023-09-27 14:22:45 +0900

    shopsテーブルに関連していなかった不要なreference型のカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_051301) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_052146) do
   create_table "articles", force: :cascade do |t|
     t.string "title", default: "", null: false
     t.datetime "start_at", null: false
@@ -78,8 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_051301) do
     t.string "shop_images", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -98,5 +96,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_051301) do
 
   add_foreign_key "contacts", "users"
   add_foreign_key "job_entries", "users"
-  add_foreign_key "shops", "users"
 end
