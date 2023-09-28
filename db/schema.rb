e5bdef0aaa54cd89a63599a54b0ee1f237b30cc2
岩橋拓海 <minton@kin.local>
2023-09-28 10:44:28 +0900

    テーブルの updated_by カラムと users テーブルの id カラムを外部キーとして関連付けるため、update_by カラムを外部キーとして関連付けていたエラーを修正し、不要になったuserテーブルのupdate_byカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_28_010049) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_28_012534) do
   create_table "articles", force: :cascade do |t|
     t.string "title", default: "", null: false
     t.datetime "start_at", null: false
@@ -103,7 +103,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_010049) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "phone_number"
-    t.integer "update_by"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
